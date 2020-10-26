a7c9060164fed5bceb0c119f9507592711bff4da
Justin Obara <obara.justin@gmail.com>
2020-10-26 11:19:23 -0400

    NOJIRA: Update due to rename of default branch to main

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,13 @@
 /*
 Copyright The Eslint-Config-Fluid copyright holders
 See the AUTHORS.md file at the top-level directory of this distribution and at
-https://github.com/fluid-project/eslint-config-fluid/raw/master/AUTHORS.md.
+https://github.com/fluid-project/eslint-config-fluid/raw/main/AUTHORS.md.
 
 Licensed under the New BSD license. You may not use this file except in
 compliance with this License.
 
 You may obtain a copy of the License at
-https://github.com/fluid-project/eslint-config-fluid/raw/master/LICENSE.txt
+https://github.com/fluid-project/eslint-config-fluid/raw/main/LICENSE.txt
 */
 
 /* eslint-env node */
